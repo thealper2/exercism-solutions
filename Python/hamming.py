c5e6512aa9695ea2eaafccdd1409f955791965aa
Alper Karaca <thisiseternity12@gmail.com>
2026-03-13 18:42:10 +0300

    Hamming

---
+++ Python/hamming.py
@@ -0,0 +1,10 @@
+def distance(strand_a, strand_b):
+    if len(strand_a) != len(strand_b):
+        raise ValueError("Strands must be of equal length.")
+
+    hamming = 0
+    for a, b in zip(strand_a, strand_b):
+        if a != b:
+            hamming += 1
+
+    return hamming
