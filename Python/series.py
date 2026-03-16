c56b20b6ab6b2490f217208c1ae242e33cdf4bfa
Alper Karaca <thisiseternity12@gmail.com>
2026-03-16 22:50:16 +0300

    Series

---
+++ Python/series.py
@@ -0,0 +1,14 @@
+def slices(series, length):
+    if length == 0:
+        raise ValueError("slice length cannot be zero")
+
+    if length < 0:
+        raise ValueError("slice length cannot be negative")
+
+    if not series:
+        raise ValueError("series cannot be empty")
+
+    if len(series) < length:
+        raise ValueError("slice length cannot be greater than series length")
+
+    return [series[i:i+length] for i in range(len(series) - length + 1)]
