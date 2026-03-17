2ce9af5bc3a0f1363a6ab17fddd516489c6fbeb3
Alper Karaca <thisiseternity12@gmail.com>
2026-03-17 13:19:31 +0300

    Roman Numerals

---
+++ Python/roman_numerals.py
@@ -0,0 +1,23 @@
+def roman(number):
+    result = ""
+    roman = {
+        1: 'I', 
+        4: 'IV', 
+        5: 'V', 
+        9: 'IX', 
+        10: 'X', 
+        40: 'XL',
+        50: 'L', 
+        90: 'XC', 
+        100: 'C', 
+        400: 'CD', 
+        500: 'D', 
+        900: 'CM', 
+        1000: 'M',
+    }
+    for value, symbol in sorted(roman.items(), reverse=True):
+        while number >= value:
+            result += symbol
+            number -= value
+
+    return result
